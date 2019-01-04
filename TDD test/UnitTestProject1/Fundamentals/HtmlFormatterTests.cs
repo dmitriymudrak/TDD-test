ab4ed4ef74e4425d3515443bf6ee852990517879
DESKTOP-IA8B0AR\developer <dmitriymudrak@gmail.com>
2019-01-04 12:35:14 +0200

    s

---
+++ TDD test/UnitTestProject1/Fundamentals/HtmlFormatterTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace UnitTestProject1.Fundamentals
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEnclosedElementWithStrongTag()
+        {
+            var formatter = new HtmlFormatter();
+            var result = formatter.FormatAsBold("someString");
+
+            //specific
+            //Assert.That(result, Is.EqualTo("<strong>someString</strong>"));
+
+            //more general
+            Assert.That(result, Does.StartWith("<strong>"));
+            Assert.That(result, Does.EndWith("</strong>"));
+            Assert.That(result, Does.Contain("someString"));
+        }
+    }
+}
---
+++ TDD test/UnitTestProject1/MathTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class MathTests
+    {
+        private Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+        
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var result = _math.Add(1, 1);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        [TestCase(1,2,2)]
+        [TestCase(3, 3, 3)]
+        [TestCase(5, 1, 5)]
+        public void Max_WhenCalled_ReturnGreater(int a, int b, int expectedResult)
+        {
+            var result = _math.Max(a, b);
+            
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
